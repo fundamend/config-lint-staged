79042b1297179b03b0eae6f433168c71897c2320
- <gitkraken@mail.decouplr.com>
2021-11-30 08:37:24 +0100

    feat: upgrade lint-staged and convert to ESM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-module.exports = {
+export default {
 	'*.*': ['prettier --write', 'jest --bail --runInBand --findRelatedTests'],
 	'*.js': 'eslint',
 	'*.css': 'stylelint',
-	'*.md': 'remark --ignore-path .gitignore',
-	'*.{html,njk]': 'rehype --ignore-path .gitignore'
+	'*.md': 'remark --ignore-path .gitignore'
+	//'*.{html,njk]': 'rehype --ignore-path .gitignore'
 };
